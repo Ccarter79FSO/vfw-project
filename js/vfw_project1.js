c6129f9cb4e9e8102de7e523773a45eddb6fcf9a
Christopher Carter <Ccarter79@fullsail.edu>
2012-01-12 03:00:01 -0500

    Created .css & .js files for project week2. Added JS code to add functionality to HTML form. trying to get the getCheckBoxValue1 function to work. Created function to getData from local storage and display on screen. Created the clear data function, and toggle controls function. adding css styles to my web app.

--- js/vfw_project1.js
+++ js/vfw_project1.js
@@ -1,6 +1,6 @@
 //Wait until the DOM is ready.
 window.addEventListener("DOMContentLoaded", function(){
-	alert(localStorage.value(0));
+                                      
 	
 	//getElementById Function
 	function $(x){
@@ -25,25 +25,26 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 	
 	//find value of selected radio button
-	function getSelectedRadio1(){
+	function getSelectedRadio(){
 		var radios = document.forms[0].platform;
 		for (i=0; i<radios.length; i++) {
 			if (radios[i].checked) {
-				platformValue = radios[i];
+				platformValue = radios[i].value;
 			}
 		}
 	}
 	
-	function getSelectedRadio2(){
-		var radios = document.forms[0].genre;
-		for (i=0; i<radios.length; i++) {
-			if (radios[i].checked) {
-				genreValue = radios[i];
+	function getCheckBoxValue1(){
+		var checkbox = document.forms[0].genre;
+		for (i=0; i<checkbox.length; i++) {
+			if (checkbox[i].checked) {
+				genreValue = checkbox[i].value;
+				genreArray.push(genreValue);
 			}
 		}
 	}
 	
-	function getCheckBoxValue(){
+	function getCheckBoxValue2(){
 		if($('fav').checked) {
 			favoriteValue = $('fav').value;
 		} else {
@@ -51,17 +52,37 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('gameForm').style.display = "none";
+				$('clear').style.display = "inline";
+				$('displayLink').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('gameForm').style.display = "block";
+				$('clear').style.display = "inline";
+				$('displayLink').style.display = "inline";
+				$('addNew').style.display = "none";
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
 	function storeData () {
 		var id 			  	= Math.floor(Math.random()*100000001);
 		//gather up all our form field values aand store in an object
 		//object properties are going to contain and array with the form label and input value
-		getSelectedRadio1();
-		getSelectedRadio2();
-		getCheckBoxValue();
+		getSelectedRadio();
+		getCheckBoxValue1();
+		getCheckBoxValue2();
 		var item		  	= {};
 			item.gname		= ["Game Name:", $('gname').value];
 			item.platform	= ["Platform:", platformValue];
-			item.genre		= ["Genre:", genreValue];
+			item.genre		= ["Genre:", genreArray];
 			item.fav		= ["Favorite:", favoriteValue];
 			item.rating		= ["Rating:", $('rating').value];
 			item.pdate		= ["Purchase Date:", $('pdate').value];
@@ -73,22 +94,62 @@ window.addEventListener("DOMContentLoaded", function(){
 			
 	}
 	
+	function getData(){
+		toggleControls("on"); 
+		if (localStorage.length===0){
+			alert("There is no data in storage!");
+		}
+		//write data from local storage to the browswer
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for (var i=0, j=localStorage.length; i<j; i++){
+			var makeLi = document.createElement('li');
+			makeList.appendChild(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//convert string from local storage value back to an object using JSON.parse()
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeLi.appendChild(makeSubList);
+			for(var n in obj){
+				var makeSubLi = document.createElement('li');
+				makeSubList.appendChild(makeSubLi);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+			}
+		}
+	
+	}
+	
+	function clearLocal(){
+		if (localStorage.lenth===0){
+			alert("There is no data to clear.")
+		}else{
+			localStorage.clear();
+			alert("All data have been deleted.");
+			window.location.reload();
+			return false;
+		}
+	}
 	
 	//variable defaults
 	var numPlayers = ["--Number of Players--", "1, or Single Player only", "2 Players", "3 Players", "4 or more Players"],
 		platformValue,
 		genreValue,
+		genreArray = [],
 		favoriteValue = "No"
 	;
 	makeCats();
 	
 	//set link & submit click events
-/*
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click",getData);
 	var clearLink = $('clear');
 	clearLink.addEventListener("click", clearLocal);
-*/
 	var save = $('submit');
 	save.addEventListener("click", storeData);
 
