af9065848d5950443ea09e8fbd0034a83deb3d20
Christopher Carter <Ccarter79@fullsail.edu>
2012-01-18 04:58:52 -0500

    Adding more code to add edit/delete nav functionality to app.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-/*
+ /*
 Christopher Carter
-VFW Project week 2
+VFW Project week 3
 Term 0112
 */
 
@@ -116,6 +116,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		$('items').style.display = "block";
 		for (var i=0, j=localStorage.length; i<j; i++){
 			var makeLi = document.createElement('li');
+			var linksLi = document.createElement('li');
 			makeList.appendChild(makeLi);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
@@ -128,9 +129,114 @@ window.addEventListener("DOMContentLoaded", function(){
 				makeSubList.appendChild(makeSubLi);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			}
+			makeItemLinks(localStorage.key(i),linksLi); //create edit and delete links for each item in local storage
 		}
 	
+	}
+	
+	//function to create edit and delete links for each stored item when displayed
+	function makeItemLinks(key, linksLi){
+		//add edit single item link
+		var editLink = document.createElement('a');
+		editLink.href = '#';
+		editLink.key = key;	
+		var editText = "Edit Game";
+		editLink.addEventListener('click', editItem);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		//create space between links
+		var makeSpace = document.createTextNode('\u00A0');
+		linksLi.appendChild(makeSpace);
+		
+		//add delete single item link
+		var deleteLink = document.createElement('a');
+		deleteLink.href = '#';
+		deleteLink.key = key;
+		var deleteText = "Delete Game";
+		//deleteLink.addEventListener('click', deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+	
+	//function to edit single item from local storage
+	function editItem(){
+		//grab data from item in localStorage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//show the form
+		toggleControls("off");
+		
+		//populate the form fields with the current local storage values
+		$('gname').value = item.gname[1];
+		var radios = document.forms[0].platform;
+		for(var i=0;i<radios.length;i++){
+			if(radios[i].value == "Xbox 360" && item.platform[1] == "Xbox 360"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Playstation 3" && item.platform[1] == "Playstation 3"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Nintendo Wii" && item.platform[1] == "Nintendo Wii"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Nintendo DS/DSi/3DS" && item.platform[1] == "Nintendo DS/DSi/3DS"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Other Handheld" && item.platform[1] == "Other Handheld"){
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+/*
+		var checkbox = document.forms[0].genre;
+		for (i=0; i<checkbox.length; i++) {
+			var checkBoxIndex = checkbox[i];
+			if (checkBoxIndex.value == "RPG" && item.genre[1] == "RPG") {
+				checkBoxIndex.setAttribute("checked", "checked");
+			}else if(checkBoxIndex == "FPS" && item.genre[1] == "FPS"){
+				checkBoxIndex.setAttribute("checked", "checked");
+			}else if(checkBoxIndex == "Action/Adventure" && item.genre[1] == "Action/Adventure"){
+				checkBoxIndex.setAttribute("checked", "checked");
+			}else if(checkBoxIndex == "RTS" && item.genre[1] == "RTS"){
+				checkBoxIndex.setAttribute("checked", "checked");
+			}else if(checkBoxIndex == "Simulation" && item.genre[1] == "Simulation"){
+				checkBoxIndex.setAttribute("checked", "checked");
+			}else if(checkBoxIndex == "Sports" && item.genre[1] == "Sports"){
+				checkBoxIndex.setAttribute("checked", "checked");
+			}else if(checkBoxIndex == "Casual" && item.genre[1] == "Casual"){
+				checkBoxIndex.setAttribute("checked", "checked");
+			}
+		}
+*/
+/*
+		if(item.genre[1] == "RPG" || "FPS" || "Action/Adventure" || "RTS" || "Simulation" || "Sports" || "Casual"){
+			$('genre').setAttribute('checked','checked');
+		}
+*/
+		var checkbox = document.forms[0].genre;
+		for (var i=0; i<checkbox.length; i++){
+			if (checkbox[i] == "RPG" && item.genre[1] == "RPG") {
+				checkbox[i].setAttribute("checked", "checked");
+			}else if(checkbox[i] == "FPS" && item.genre[1] == "FPS"){
+				checkbox[i].setAttribute("checked", "checked");
+			}else if(checkbox[i] == "Action/Adventure" && item.genre[1] == "Action/Adventure"){
+				checkbox[i].setAttribute("checked", "checked");
+			}else if(checkbox[i] == "RTS" && item.genre[1] == "RTS"){
+				checkbox[i].setAttribute("checked", "checked");
+			}else if(checkbox[i] == "Simulation" && item.genre[1] == "Simulation"){
+				checkbox[i].setAttribute("checked", "checked");
+			}else if(checkbox[i] == "Sports" && item.genre[1] == "Sports"){
+				checkbox[i].setAttribute("checked", "checked");
+			}else if(checkbox[i] == "Casual" && item.genre[1] == "Casual"){
+				checkbox[i].setAttribute("checked", "checked");
+			}
+		}									
+		if(item.fav[1] == "Fav"){
+			$('fav').setAttribute('checked','checked');
+		}
+		$('rating').value = item.rating[1];
+		$('pdate').value = item.pdate[1];
+		$('players').value = item.players[1];
+		$('notes').value = item.notes[1];	
 	}
 	
 	function clearLocal(){
